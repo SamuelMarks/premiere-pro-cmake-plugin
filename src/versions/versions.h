497838e20727eb18b91d734b9b2381fd5a3e4f16
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-07-19 19:19:52 +1000

    [src/versions] Resolve linker error on MSVC through manually editing generated export header (wasn't C compatible; reported bug: https://gitlab.kitware.com/cmake/cmake/-/issues/22442); [CMakeLists.txt,src/pp-plugin/CMakeLists.txt] More compatibility consistencies with Premiere Pro's sample projects; remove debug statements; use default shared lib flag

--- src/versions/versions.h
+++ src/versions/versions.h
@@ -6,8 +6,17 @@
 #include <openssl/crypto.h>
 #include "versions_export.h"
 
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
 extern VERSIONS_EXPORT const wchar_t *VERSIONS[][2];
 extern VERSIONS_EXPORT const size_t VERSIONS_N;
+
 uLong VERSIONS_EXPORT get_hash();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* PP_VERSIONS_H */
